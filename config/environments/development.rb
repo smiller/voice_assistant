e950a95ad4ed80ade31a9e1d874a5c6c090ecd66
Sean Miller <sean@seanmiller.ca>
2026-02-24 18:08:01 -0500

    Revert "chore: use solid_queue adapter in development"
    This reverts commit 5c293e055f7020c149d8ebf441502f57632e9eec.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,8 +3,6 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.active_job.queue_adapter = :solid_queue
-
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
